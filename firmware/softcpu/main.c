bc7f76737b02ff41c3f208eb1c9e390b85cdd5cd
Joel Stanley <joel@jms.id.au>
2016-04-12 10:15:23 +0930

    firmware: Create simplified firmware
    This is for testing the minispartan6 and is not intended to go upstream.
    It prints version information and then jumps back to the bios.
    
    Signed-off-by: Joel Stanley <joel@jms.id.au>

--- firmware/softcpu/main.c
+++ firmware/softcpu/main.c
@@ -9,71 +9,35 @@
 #include <hw/flags.h>
 #include <console.h>
 
-#include "config.h"
-#include "ci.h"
-#include "processor.h"
-#include "encoder.h"
-#include "pattern.h"
-#include "hdmi_out0.h"
-#include "hdmi_out1.h"
-#include "fx2.h"
 #include "version.h"
+
+extern void boot_helper(unsigned int r1, unsigned int r2, unsigned int r3,
+		unsigned int addr);
+
+static void __attribute__((noreturn)) boot(unsigned int r1, unsigned int r2,
+		unsigned int r3, unsigned int addr) 
+{
+	uart_sync();
+	irq_setmask(0);
+	irq_setie(0);
+	flush_cpu_icache();
+	boot_helper(r1, r2, r3, addr);
+	while(1);
+}
 
 int main(void)
 {
 	irq_setmask(0);
 	irq_setie(1);
 	uart_init();
-#ifdef CSR_HDMI_OUT0_I2C_W_ADDR
-	hdmi_out0_i2c_init();
-#endif
-#ifdef CSR_HDMI_OUT1_I2C_W_ADDR
-	hdmi_out1_i2c_init();
-#endif
 
 	puts("\r\nHDMI2USB firmware  http://timvideos.us/");
 	print_version();
 
-	config_init();
 	time_init();
-	processor_init();
-	processor_start(config_get(CONFIG_KEY_RESOLUTION));
 
-	// Set HDMI Output 0 to be pattern
-#ifdef CSR_HDMI_OUT0_BASE
-	processor_set_hdmi_out0_source(VIDEO_IN_PATTERN);
-#endif
-	// Set HDMI Output 1 to be pattern
-#ifdef CSR_HDMI_OUT1_BASE
-	processor_set_hdmi_out1_source(VIDEO_IN_PATTERN);
-#endif
-	processor_update();
-
-	// Reboot the FX2 chip into HDMI2USB mode
-#ifdef CSR_FX2_RESET_OUT_ADDR
-	fx2_init();
-#endif
-
-	// Set Encoder to be pattern
-#ifdef ENCODER_BASE
-	processor_set_encoder_source(VIDEO_IN_PATTERN);
-	encoder_enable(1);
-	processor_update();
-#endif
-	ci_prompt();
-	while(1) {
-		processor_service();
-		ci_service();
-
-#ifdef CSR_FX2_RESET_OUT_ADDR
-		fx2_service(true);
-#endif
-
-/* XXX FIX DDR conflict between DMA and L2 cache */
-#if 0
-		pattern_service();
-#endif
-	}
+	puts("returning to bios\n");
+	boot(0, 0, 0, 0);
 
 	return 0;
 }
